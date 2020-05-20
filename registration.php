a18884b4b18bf7266ffd444552d5af4da7b34669
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-20 12:09:44 +0530

    initial commmit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Responsive Banner Slider Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_Bannerslider
+ * @copyright  Copyright © 2006-2019 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Bannerslider',
+    __DIR__
+);
